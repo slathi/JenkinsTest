c3b26e85259429d54ccb2535c8ff5357098235af
slathi <lathi.saumya@gmail.com>
2017-02-10 18:12:45 +0530

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ exports.config = {
 		directConnect:true,
 		framework: 'jasmine',
 
-		//seleniumAddress: 'http://localhost:4444/wd/hub',
+		seleniumAddress: 'http://localhost:4444/wd/hub',
 		specs: ['w3school.js'],
 
 		jasmineNodeOpts: {defaultTimeoutInterval: 600000},
