19afebec805ae8fe59ce23f4a11f8a34e20060d4
slathi <lathi.saumya@gmail.com>
2017-02-10 17:54:54 +0530

    no message

---
+++ conf.js
@@ -0,0 +1,15 @@
+exports.config = {
+		directConnect:true,
+		framework: 'jasmine',
+
+		//seleniumAddress: 'http://localhost:4444/wd/hub',
+		specs: ['w3school.js'],
+
+		jasmineNodeOpts: {defaultTimeoutInterval: 600000},
+		onPrepare : function (){
+			browser.driver.manage().window().maximize();
+		}
+		// capabilities: {
+		// browserName: 'firefox'
+		// }
+};
